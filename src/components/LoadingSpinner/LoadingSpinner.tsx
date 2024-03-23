26c5c3e4a227ce132f9a1e8cf425b5816ff07213
Tyler Earls <tyler.a.earls@gmail.com>
2024-03-23 11:30:41 -0500

    disable css modules

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,5 +1,6 @@
-import styles from "./LoadingSpinner.module.css";
+// import styles from "./LoadingSpinner.module.css";
 
 export default function LoadingSpinner() {
-  return <div className={styles["loading-spinner"]} />;
+  // return <div className={styles["loading-spinner"]} />;
+  return <div />;
 }
