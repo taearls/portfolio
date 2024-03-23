b930649eafd95c704823b5c768e4282a0d008e04
Tyler Earls <tyler.a.earls@gmail.com>
2024-03-23 11:53:23 -0500

    try fixing build error again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "styles/global.css";
+import "@/styles/global.css";
 import { Header, Footer, PageContainer, Body } from "@/components/layout";
 
 export const metadata: Metadata = {
