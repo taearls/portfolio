ca22f624801833dd417cb70a242635bebf1df0f0
Tyler Earls <tyler.a.earls@gmail.com>
2025-05-10 10:57:04 -0500

    fix condition

--- wdyr.ts
+++ wdyr.ts
@@ -1,7 +1,7 @@
 import whyDidYouRender from "@welldone-software/why-did-you-render";
 import React from "react";
 
-if (process.env.NODE_ENV === "development") {
+if (import.meta.env.MODE === "development") {
   whyDidYouRender(React, {
     exclude: [/^BrowserRouter/, /^Link/, /^Route/],
     include: [/.*/],
